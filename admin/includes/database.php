6d548600cffad60e39aa80f20de81eed70931825
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-03-08 15:15:32 +0100

    Automatic DB Connection Setup

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -3,8 +3,13 @@
 
     class Database{
         public $connection;
+
+        function __construct(){
+            $this -> open_db_connection();
+        }
+
         public function open_db_connection(){
-            this -> connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+            $this -> connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
             if(mysqli_connect_errno()){
                 die("Database connection failed! " . mysqli_error());
@@ -13,5 +18,4 @@
     }
 
     $database = new Database();
-    $database -> open_db_connection();
 ?>
