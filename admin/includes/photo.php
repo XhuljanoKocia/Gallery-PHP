fc9a83242a46e5080cad01cbc981a4be392bce65
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-03-15 15:47:19 +0100

    Adding Class Properties

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -1,3 +1,12 @@
 <?php
-    class Photo extends Db_object{}
+    class Photo extends Db_object{
+        protected static $db_table = "photos";
+        protected static $db_table_fields = array('photo_id', 'title', 'description', 'filename', 'type', 'size');
+        public $photo_id;
+        public $title;
+        public $description;
+        public $filename;
+        public $type;
+        public $size;
+    }
 ?>
