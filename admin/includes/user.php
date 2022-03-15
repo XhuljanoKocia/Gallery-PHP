e2ba96a6801cadd81d12259a188def91a88fd9f6
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-03-15 14:13:15 +0100

    Abstracting the remaining methods

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -8,14 +8,14 @@
         public $first_name;
         public $last_name;
 
-        public static function find_all_users(){
-            return self::find_this_query("SELECT * FROM users");
+        public static function find_all(){
+            return self::find_this_query("SELECT * FROM " . self::$db_table . " ");
         }
 
-        public static function find_user_by_id($user_id){
+        public static function find_by_id($user_id){
             global $database;
 
-            $the_result_array = self::find_this_query("SELECT * FROM users WHERE id = $user_id LIMIT 1");
+            $the_result_array = self::find_this_query("SELECT * FROM " . self::$db_table . " WHERE id = $user_id LIMIT 1");
 
             return !empty($the_result_array) ? array_shift($the_result_array) : false;
         }
@@ -39,7 +39,7 @@
             $username = $database -> escape_string($username);
             $password = $database -> escape_string($password);
 
-            $sql = "SELECT * FROM users WHERE username = '{$username}' AND password = '{$password}' LIMIT 1";
+            $sql = "SELECT * FROM " . self::$db_table . " WHERE username = '{$username}' AND password = '{$password}' LIMIT 1";
 
             $the_result_array = self::find_this_query($sql);
 
