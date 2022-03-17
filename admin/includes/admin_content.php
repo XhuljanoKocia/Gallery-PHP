8eee7bc49d339e9197b886673315ad91a13a6533
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-03-17 15:24:08 +0100

    Small Fixes

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -7,10 +7,6 @@
                 Admin
                 <small>Subheading</small>
             </h1>
-            <?php
-                $user = User::find_by_id(5);
-                echo $user -> username;
-            ?>
             <ol class="breadcrumb">
                 <li>
                     <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
