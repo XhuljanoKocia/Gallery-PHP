40891b6e38842e3a7cd1f782c35431657b900ce3
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-03-21 10:45:03 +0100

    Displaying & Making Comments - FRONT-END

--- photo.php
+++ photo.php
@@ -23,7 +23,7 @@
         $body = "";
     }
 
-    //Comment::find_the_comments($photo -> id);
+    $comments = Comment::find_the_comments($photo -> id);
 ?>
 
 <!DOCTYPE html>
@@ -148,18 +148,22 @@
 
                 <!-- Posted Comments -->
 
+                <?php foreach($comments as $comment) : ?>
+
                 <!-- Comment -->
                 <div class="media">
                     <a class="pull-left" href="#">
                         <img class="media-object" src="http://placehold.it/64x64" alt="">
                     </a>
                     <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
+                        <h4 class="media-heading"><?php echo $comment -> author; ?>
+                            <small><?php echo $date = date('Y-m-d H:i:s'); ?></small>
                         </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        <?php echo $comment -> body; ?>
                     </div>
                 </div>
+
+                <?php endforeach; ?>
 
             </div>
 
