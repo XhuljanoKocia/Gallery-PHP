d87c11b481af7707ada5322b203eeb7f47e7f499
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-03-21 22:36:11 +0100

    Setting Up Index to Display Photos Part #2

--- index.php
+++ index.php
@@ -6,15 +6,15 @@
         <div class="row">
             <!-- Blog Entries Column -->
             <div class="col-md-12">
-                <?php foreach($photos as $photo): ?>
-                    <div class="thumbnails row">
+                <div class="thumbnails row">
+                    <?php foreach($photos as $photo): ?>
                         <div class="col-xs-6 col-md-3">
                             <a class="thumbnail" href="">
-                                <img src="" alt="">
+                                <img src="admin/<?php echo $photo -> picture_path(); ?>" alt="">
                             </a>
                         </div>
-                    </div>
-                <?php endforeach; ?>
+                    <?php endforeach; ?>
+                </div>
             </div>
 
 <?php include("includes/footer.php"); ?>
