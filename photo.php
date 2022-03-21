c17262c7dfd4f240b668d699be6486e58fbab86c
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-03-21 10:21:40 +0100

    Pulling Data From Form Part # 1

--- photo.php
+++ photo.php
@@ -8,7 +8,14 @@
     $photo = Photo::find_by_id($_GET['id']);
 
     if(isset($_POST['submit'])){
-        echo "Hello";
+        $author = trim($_POST['author']);
+        $body = trim($_POST['body']);
+
+        $new_comment = Comment::create_comment($photo -> id, $author, $body);
+
+        if($new_comment && $new_comment -> save()){
+            redirect("photo.php?id={$photo -> id}");
+        }
     }
 ?>
 
