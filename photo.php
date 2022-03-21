822069c5e5f4510573a0e156bd7afb30c496b1bb
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-03-21 10:12:51 +0100

    Including Our Classes in Photo.php FRONT-END

--- photo.php
+++ photo.php
@@ -1,4 +1,12 @@
 <?php
+    require_once("admin/includes/init.php");
+
+    if(empty($_GET['id'])){
+        redirect("index.php");
+    }
+
+    $photo = Photo::find_by_id($_GET['id']);
+
     if(isset($_POST['submit'])){
         echo "Hello";
     }
