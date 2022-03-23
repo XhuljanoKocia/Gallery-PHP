e17aa53af4bdba5db5446b0cf1400f57fc66a2d6
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-03-23 12:09:58 +0100

    Enabling the Selection Button with jQuery

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
+    $(".modal_thumbnails").click(function(){
+        $("#set_user_image").prop('disabled', false);
+    });
+
     tinymce.init({selector:'textarea'});
 });
